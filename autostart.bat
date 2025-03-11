115a33357772de848cc5a3b82e622d0d3e1379f8
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-11 15:42:59 +0100

    Update autostart.bat

--- autostart.bat
+++ autostart.bat
@@ -1,6 +1,6 @@
 @echo off
 set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "batFilePath=C:\users\%USERNAME%\kl2\runos.bat"
+set "batFilePath=C:\users\%USERNAME%\kl2.1\runos.bat"
 
 :: Check if the batch file already exists in the Startup folder
 if not exist "%startupFolder%\runos.bat" (
