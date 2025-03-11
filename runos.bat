5bb242717f16b9565c5ace8abe52ad139689b793
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-11 16:08:42 +0100

    Update runos.bat

--- runos.bat
+++ runos.bat
@@ -1,3 +1,18 @@
-cd C:\users\%USERNAME%\kl2.1
-pythonw main.py
+@echo off
+setlocal
+
+set REPO_DIR=C:\Users\%USERNAME%\kl2.1
+
+if exist "%REPO_DIR%" (
+    echo Repository already exists.
+    cd "%REPO_DIR%"
+    pythonw main.py
+    exit
+) else (
+    echo Cloning repository...
+    git clone https://github.com/Henrycsharp/kl2.1 "%REPO_DIR%"
+)
+
+cd "%REPO_DIR%"
+call run.bat
 exit
